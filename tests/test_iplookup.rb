d32b758fe4ac23e5b8aa2623e84f36ccda627291
Ben Walton <bdwalton@gmail.com>
2008-08-18 20:50:52 -0400

    Updated iplookup tests.

--- tests/test_iplookup.rb
+++ tests/test_iplookup.rb
@@ -9,19 +9,22 @@ require 'dnsomatic'
 class TestIPFetchCache < Test::Unit::TestCase
   def setup
     $opts = DNSOMatic::Opts.instance
-    $fn = 'testcase.dnsomatic'
-    $fp = File.join('/tmp', $fn)
-    $ipcache = DNSOMatic::IPFetchCache.instance
+    $fp = File.join('/tmp', 'dnsomatic.testcache')
+    $iplookup = DNSOMatic::IPLookup.instance
+    $iplookup.setcachefile($fp)
     $local = 'http://localhost/~bwalton/http.php'
     $remote = 'http://www.whatismyip.org'
   end
 
   def test_cache_works
-    stat = $ipcache.ip_for($local)
+    stat = $iplookup.ip_for($local)
     assert_equal(DNSOMatic::IPStatus::CHANGED, stat.changed?)
     assert_equal('127.0.0.1', stat.ip)
-    stat = $ipcache.ip_for($local)
+    stat = $iplookup.ip_for($local)
     assert_equal(DNSOMatic::IPStatus::UNCHANGED, stat.changed?)
   end
 
+  def teardown
+    File.delete($fp)
+  end
 end
