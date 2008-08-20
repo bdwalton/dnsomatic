fbbfe65f29e895dce1d718bb7aa380a3609551f1
Ben Walton <bdwalton@gmail.com>
2008-08-19 21:56:52 -0400

    changed var name in iplookup to make more sense

--- lib/dnsomatic/iplookup.rb
+++ lib/dnsomatic/iplookup.rb
@@ -29,7 +29,7 @@ module DNSOMatic
       fn = 'dnsomatic-' + Process.uid.to_s + '.cache'
       @cache_file = File.join(ENV['TEMP'] || '/tmp', fn)
 
-      @fetch_map = {}
+      @cache = {}
       @persist = true
     end
 
@@ -43,16 +43,16 @@ module DNSOMatic
       #implement a simple cache to prevent making multiple http requests
       #to the same remote agent (in the case where a user defines multiple
       #updater stanzas that use the same ip fetch url).
-      if @fetch_map[url] and Time.now - @fetch_map[url][:time] <= $opts.interval
-	ip = @fetch_map[url][:ip]
-	time = @fetch_map[url][:time]
+      if @cache[url] and Time.now - @cache[url][:time] <= $opts.interval
+	ip = @cache[url][:ip]
+	time = @cache[url][:time]
 	stat = IPStatus::UNCHANGED
 	Logger::log("Returned cached IP #{ip} from #{url}")
       else  #unknown or expired
-	prev_ip = @fetch_map[url].nil? ? '' : @fetch_map[url][:ip]
+	prev_ip = @cache[url].nil? ? '' : @cache[url][:ip]
 	ip = DNSOMatic::http_fetch(url)
 	stat, time = case ip
-	      when prev_ip: [IPStatus::UNCHANGED, @fetch_map[url][:time]]
+	      when prev_ip: [IPStatus::UNCHANGED, @cache[url][:time]]
 	      else [IPStatus::CHANGED, Time.now]
 	    end
 	Logger::log("Feched IP #{ip} from #{url}")
@@ -65,7 +65,7 @@ module DNSOMatic
 	raise(DNSOMatic::Error, msg)
       end
 
-      @fetch_map[url] = { :ip => ip, :time => time }
+      @cache[url] = { :ip => ip, :time => time }
       save()  #ensure that we get spooled to disk.
       IPStatus.new(ip, stat)
     end
@@ -73,12 +73,12 @@ module DNSOMatic
     private
     def load
       if File.exists?(@cache_file) and @persist
-	@fetch_map = DNSOMatic::yaml_read(@cache_file)
+	@cache = DNSOMatic::yaml_read(@cache_file)
       end
     end
 
     def save
-      DNSOMatic::yaml_write(@cache_file, @fetch_map) if @persist 
+      DNSOMatic::yaml_write(@cache_file, @cache) if @persist 
     end
   end
 end
