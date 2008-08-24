58e1c6527fdf1351c165e1d7fed318557f3ef34c
Ben Walton <bdwalton@gmail.com>
2008-08-24 19:25:24 -0400

    Updated tests to handle newly improved object encapsulation between updater -> iplookup -> ipstatus.

--- tests/test_iplookup.rb
+++ tests/test_iplookup.rb
@@ -19,35 +19,35 @@ class TestIPLookup < Test::Unit::TestCase
   end
 
   def test_cache_works
-    stat = $iplookup.ip_for($local)
+    stat = $iplookup.ip_from_url($local)
     assert_equal(DNSOMatic::IPStatus::CHANGED, stat.changed?)
     assert_equal($local_ip, stat.ip)
-    stat = $iplookup.ip_for($local)
+    stat = $iplookup.ip_from_url($local)
     assert_equal(DNSOMatic::IPStatus::UNCHANGED, stat.changed?)
   end
 
   def test_different_source_returns_new_val
-    stat = $iplookup.ip_for($random)
+    stat = $iplookup.ip_from_url($random)
     assert_equal(DNSOMatic::IPStatus::CHANGED, stat.changed?)
   end
 
   def test_known_ip_change_still_prefers_cache
-    stat = $iplookup.ip_for($random)
+    stat = $iplookup.ip_from_url($random)
     assert_equal(DNSOMatic::IPStatus::UNCHANGED, stat.changed?)
   end
 
   def test_expiration_with_known_change
-    stat = $iplookup.ip_for($random)
+    stat = $iplookup.ip_from_url($random)
     assert_equal(DNSOMatic::IPStatus::UNCHANGED, stat.changed?)
     $opts.parse(%w(-i 2)) #change expiration to 2s.
     sleep(3)  #make sure we pass the expiration time we just set.
-    stat = $iplookup.ip_for($random)
+    stat = $iplookup.ip_from_url($random)
     assert_equal(DNSOMatic::IPStatus::CHANGED, stat.changed?)
   end
 
   def test_persist_stores_to_file
     $iplookup.persist = true
-    stat = $iplookup.ip_for($random)
+    stat = $iplookup.ip_from_url($random)
     assert(File.exists?($fp), "${fp} doesn't exist when persist is enabled.")
   end
 
