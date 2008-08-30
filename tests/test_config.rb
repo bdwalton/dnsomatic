012147d23e9824e47ff7b7a7af5140f86c3e3f28
Ben Walton <bdwalton@gmail.com>
2008-08-29 21:35:27 -0400

    Added more tests for DNSOMatic::Config and some config files to assist this testing.

--- tests/test_config.rb
+++ tests/test_config.rb
@@ -9,6 +9,16 @@ require 'dnsomatic'
 class TestConfig < Test::Unit::TestCase
   def setup
     $cfd = File.join(Dir.pwd, 'confs')
+    $orig_home = ENV['HOME']
+  end
+
+  def test_no_exception_with_basic_default_conf
+    assert_nothing_raised { DNSOMatic::Config.new }
+  end
+
+  def test_exception_on_no_config_file
+    ENV['HOME'] = '/tmp'
+    assert_raises(DNSOMatic::Error) { DNSOMatic::Config.new }
   end
 
   def test_non_existent_conf_raises_exception
@@ -31,6 +41,23 @@ class TestConfig < Test::Unit::TestCase
     assert_nothing_raised { DNSOMatic::Config.new(conf) }
   end
 
+  def test_1_def_and_3_alternates_gives_3_updaters
+    conf = File.join($cfd, '3_good_stanzas.cf')
+    c = DNSOMatic::Config.new(conf)
+    assert_equal(3, c.updaters.length)
+  end
+
+  def test_3_stanzas_no_def_no_u_p_raises
+    conf = File.join($cfd, '3_good_stanzas_no_def.cf')
+    assert_raises(DNSOMatic::Error) { DNSOMatic::Config.new(conf) }
+  end
+
+  def test_no_defs_no_u_p_raises
+    conf = File.join($cfd, '1_stanza_no_defs_no_u_p.cf')
+    assert_raises(DNSOMatic::Error) { DNSOMatic::Config.new(conf) }
+  end
+
   def teardown
+    ENV['HOME'] = $orig_home
   end
 end
